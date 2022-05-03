9f095daf1b9bb7b97242fa97d64dee8d3dedf1e2
João Victor Amaral <joao.amaral@squadra.com.br>
2022-05-02 22:01:19 -0300

    Update Lista 3 - Exercicio 11

--- Lista 3 - Exercicio 11.c
+++ Lista 3 - Exercicio 11.c
@@ -5,13 +5,32 @@ void DesenhaLinha();
 void DesligaPrograma();
 int main (){
 
+
+    int idade, quantidadeMulheres = 0, quantidadeHomens = 0, numeroDePessoas;
+    char sexo;
+    float altura;
+    register int cnt = 0;
+
     //Titulo do programa
     DesenhaLinha();
     printf("\tLista 3 - Exercicio 11\n");
     DesenhaLinha();
     printf("\n\n");
 
-    
+    scanf(" %d",&numeroDePessoas);
+
+    for (cnt = 0; cnt < numeroDePessoas; cnt++){
+        scanf(" %c",&sexo);
+        scanf(" %d",&idade);
+        if ((idade >= 20 && idade <= 35) && sexo == 'F')
+            quantidadeMulheres++;
+        scanf(" %f",&altura);
+        if ((altura > 1.8) && sexo == 'M')
+            quantidadeHomens++;
+        fflush(stdin);
+    }
+
+    printf("%d %d", quantidadeHomens, quantidadeMulheres);
 
 
 
