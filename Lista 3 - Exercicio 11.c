208c68cf6401b8bc58452c452398c7e6af5b40e1
João Victor Amaral <joao.amaral@squadra.com.br>
2022-05-02 22:27:48 -0300

    Adicionei o calculo da variancia

--- Lista 3 - Exercicio 11.c
+++ Lista 3 - Exercicio 11.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <math.h>
 
 void DesenhaLinha();
 void DesligaPrograma();
 int main (){
 
 
-    int idade, quantidadeMulheres = 0, quantidadeHomens = 0, numeroDePessoas;
+    int *listaIdade, quantidadeMulheres = 0, quantidadeHomens = 0, numeroDePessoas, somaIdades = 0;
     char sexo;
-    float altura;
+    float altura, mediaIdades, somatorio = 0, varianciaIdades;
     register int cnt = 0;
 
     //Titulo do programa
@@ -19,10 +20,13 @@ int main (){
 
     scanf(" %d",&numeroDePessoas);
 
+    listaIdade = (int *) malloc (numeroDePessoas*sizeof(int));
+
     for (cnt = 0; cnt < numeroDePessoas; cnt++){
         scanf(" %c",&sexo);
-        scanf(" %d",&idade);
-        if ((idade >= 20 && idade <= 35) && sexo == 'F')
+        scanf(" %d",&listaIdade[cnt]);
+        somaIdades += listaIdade[cnt];
+        if ((listaIdade[cnt] >= 20 && listaIdade[cnt] <= 35) && sexo == 'F')
             quantidadeMulheres++;
         scanf(" %f",&altura);
         if ((altura > 1.8) && sexo == 'M')
@@ -30,7 +34,17 @@ int main (){
         fflush(stdin);
     }
 
+    mediaIdades = somaIdades/numeroDePessoas;
+    cnt = 0;
+    for(cnt = 0; cnt < numeroDePessoas; cnt++){
+        somatorio += pow((listaIdade[cnt]-mediaIdades),2);
+    }
+
+    varianciaIdades = somatorio/numeroDePessoas;
+
     printf("%d %d", quantidadeHomens, quantidadeMulheres);
+
+    printf("%f",varianciaIdades);
 
 
 
