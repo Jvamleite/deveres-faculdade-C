f5e0b3d2579f4163c34c8a02b146c6c60573f776
LeonRabello <81538872+LeonRabello@users.noreply.github.com>
2022-05-09 20:37:37 -0300

    Adicionei a interação com o usuario

--- Lista 4 - Exercicio 5.c
+++ Lista 4 - Exercicio 5.c
@@ -4,24 +4,45 @@
 #define BANDEIRA1 2.50
 #define BANDEIRA2 3.00
 
+void DesenhaLinha();
+void DesligaPrograma();
 void calcula_corrida(float dist, float *b1, float *b2);
 int main (){
 
     float dist, valorCorridaB1, valorCorridabB2;
 
+    //Titulo do programa
+    DesenhaLinha();
+    printf("\tLista 4 - Exercicio 5\n");
+    DesenhaLinha();
+    printf("\n\n");
+
+    printf("Digite a distancia percorrida pelo taxi: ");
     scanf("%f",&dist);
 
     calcula_corrida(dist,&valorCorridaB1,&valorCorridabB2);
 
+    DesenhaLinha();
     printf("Valor na bandeira 1: %.2f\n",valorCorridaB1);
-
     printf("Valor na bandeira 2: %.2f\n",valorCorridabB2);
 
 
-
+    DesligaPrograma();
 
 
     return 0;
+}
+void DesenhaLinha(){
+
+printf("====================================\n");
+
+}
+void DesligaPrograma(){
+    DesenhaLinha();
+    printf("Fim do Programa. Desligando...");
+    sleep(1);
+    exit(0);
+
 }
 void calcula_corrida(float dist, float *b1, float *b2){
 
