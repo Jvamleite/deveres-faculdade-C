d26df2bb8e139bb295f74808505254fea94d2727
João Victor Amaral <joao.amaral@squadra.com.br>
2022-05-09 14:35:41 -0300

    Começando o exercício 2

---
+++ Lista 4 - Exercicio 2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+void DesenhaLinha();
+void DesligaPrograma();
+int main (){
+
+     //Titulo do programa
+    DesenhaLinha();
+    printf("\tLista 4 - Exercicio 1\n");
+    DesenhaLinha();
+    printf("\n\n");
+
+
+
+
+
+
+
+
+
+    return 0;
+}
+void DesenhaLinha(){
+
+printf("====================================\n");
+
+}
+void DesligaPrograma(){
+    DesenhaLinha();
+    printf("Fim do Programa. Desligando...");
+    sleep(1);
+    exit(0);
+
+}
