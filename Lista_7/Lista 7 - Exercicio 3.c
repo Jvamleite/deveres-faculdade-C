a8f19911f0f494243c93d77636e84fde11bdfb58
João Victor Amaral <joao.amaral@squadra.com.br>
2022-05-24 10:36:16 -0300

    Comecei exercicio 3

---
+++ Lista_7/Lista 7 - Exercicio 3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void DesenhaLinha();
+void DesligaPrograma();
+int main (){
+
+    //Titulo do programa
+    DesenhaLinha();
+    printf("\tLista  - Exercicio \n");
+    DesenhaLinha();
+    printf("\n\n");
+
+
+    
+
+
+
+    DesenhaLinha();
+
+
+
+    DesligaPrograma();
+
+    return 0;
+}
+void DesenhaLinha(){
+
+printf("====================================\n");
+
+}
+void DesligaPrograma(){
+    DesenhaLinha();
+    printf("Fim do Programa. Desligando...");
+    sleep(1);
+    exit(0);
+
+}
